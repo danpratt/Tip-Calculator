1e79a1254319e8f4d70133f7b89bd95ceed39c41
Daniel Pratt <daniel@blaumagier.com>
2017-11-07 12:40:09 -0800

    feat: added testPerformance test

--- Blue Tip CalcTests/BTCTipCalculatorTests.swift
+++ Blue Tip CalcTests/BTCTipCalculatorTests.swift
@@ -66,6 +66,7 @@ class BTCTipCalculatorTests: XCTestCase {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
+            testSetBillAmount_totalsCalculate()
         }
     }
     
