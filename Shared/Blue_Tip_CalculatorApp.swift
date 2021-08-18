e4866d9b0ab8a47f8ee5686281d53bc3d5efe89d
Daniel Pratt <daniel@blaumagier.com>
2021-08-18 19:48:22 -0400

    Initial Commit

---
+++ Shared/Blue_Tip_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Blue_Tip_CalculatorApp.swift
+//  Shared
+//
+//  Created by Daniel Pratt on 8/18/21.
+//
+
+import SwiftUI
+
+@main
+struct Blue_Tip_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
